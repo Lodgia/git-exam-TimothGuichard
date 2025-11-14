8826f9d385bc9cac0b5ba3398f27c62364479790
gutimothe <timothe.guichard@ynov.com>
2025-11-14 11:24:57 +0100

    Commit du Index handler parce que je l'ai enlevé pour faire le merge tout ça tout ça

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 )
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "route: %q", r.URL.Path)
+}
 
 func main() {
 	http.HandleFunc("/", IndexHandler)    
