2ca363a1dc43c692597a4c8ec0a5ecba73ee1478
gutimothe <timothe.guichard@ynov.com>
2025-11-14 11:15:00 +0100

    commit de la feature color modifiée afin de pouvoir merge sur dev ensuite

--- main.go
+++ main.go
@@ -6,15 +6,12 @@ import (
 	"net/http"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "route: %q", r.URL.Path)
-}
 
 func main() {
-	http.HandleFunc("/", IndexHandler)
 	http.HandleFunc("/color", ColorHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func ColorHandler(w http.ResponseWriter, r *http.Request) {  
 	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")  
 }
+
